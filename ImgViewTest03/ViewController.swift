fd4662d21d268d07934d8535bcd79854aa1de84b
dit02 <dit@imac-7.local>
2019-03-26 15:35:32 +0900

    ImgView Test

--- ImgViewTest03/ViewController.swift
+++ ImgViewTest03/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var count = 1
+    var direction = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myImageView.image = UIImage(named: "frame1.png")
+        myLabel.text = "1"
     }
 
-
+    @IBAction func changeImage(_ sender: Any) {
+        
+        if count == 5 {
+         direction = false
+        }
+        else if count == 1  {
+            direction = true
+         }
+        if direction == true {
+        count += 1
+        } else {
+            count -= 1
+        }
+        
+        myImageView.image = UIImage(named: "frame\(count).png")
+        myLabel.text = String(count)
+    }
+    
 }
 
